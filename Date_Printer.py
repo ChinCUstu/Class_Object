dab359f31cbe6a3086d423506e0777e1fe092c04
ChinCUstu <ndulue.chinedu@stu.cu.edu.ng>
2022-11-19 21:53:49 +0100

    "name_Display":               It gets string's containing a person's first and last name as separate values, and then displays their "initials", "name in address book", and "username". "Date_Printer":               It gets the user date in the format (mm/dd/yyyy) and returns it in the form Month Day, year. E.G: March 12,2018               update..........

--- Date_Printer.py
+++ Date_Printer.py
@@ -1,10 +1,10 @@
 import datetime
 
 user_date = input("Enter date(mm/dd/yyyy): ")
-user_date = user_date.lstrip()
-user_date_month = int(user_date[0:2])
-user_date_day = int(user_date[3:5])
-user_date_year = int(user_date[6:10])
+user_date = user_date.split('/')
+user_date_month = int(user_date[0])
+user_date_day = int(user_date[1])
+user_date_year = int(user_date[2])
 
 date = datetime.datetime(user_date_year, user_date_month, user_date_day)
 print(date.strftime("%B %d,%Y"))
